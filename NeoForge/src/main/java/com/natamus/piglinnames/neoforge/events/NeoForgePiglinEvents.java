b835613dca3a05b6f6669589422422ee6748ea07
Rick South <rick@natamus.com>
2025-02-12 23:21:20 +0100

    (v1.2) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/piglinnames/neoforge/events/NeoForgePiglinEvents.java
+++ NeoForge/src/main/java/com/natamus/piglinnames/neoforge/events/NeoForgePiglinEvents.java
@@ -3,11 +3,9 @@ package com.natamus.piglinnames.neoforge.events;
 import com.natamus.piglinnames.cmds.CommandPiglinnames;
 import com.natamus.piglinnames.events.PiglinEvents;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
 
-@EventBusSubscriber
 public class NeoForgePiglinEvents {
 	@SubscribeEvent
 	public static void onSpawn(EntityJoinLevelEvent e) {
