6c9155192b07ad5fb652f6de4e3cd55f05e98016
Rick South <rick@natamus.com>
2025-02-12 23:21:15 +0100

    (v1.2) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/piglinnames/forge/events/ForgePiglinEvents.java
+++ Forge/src/main/java/com/natamus/piglinnames/forge/events/ForgePiglinEvents.java
@@ -5,17 +5,15 @@ import com.natamus.piglinnames.events.PiglinEvents;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.EntityJoinLevelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-@EventBusSubscriber
 public class ForgePiglinEvents {
 	@SubscribeEvent
-	public void onSpawn(EntityJoinLevelEvent e) {
+	public static void onSpawn(EntityJoinLevelEvent e) {
 		PiglinEvents.onSpawn(e.getLevel(), e.getEntity());
 	}
 
     @SubscribeEvent
-    public void registerCommands(RegisterCommandsEvent e) {
+    public static void registerCommands(RegisterCommandsEvent e) {
     	CommandPiglinnames.register(e.getDispatcher());
     }
 }
