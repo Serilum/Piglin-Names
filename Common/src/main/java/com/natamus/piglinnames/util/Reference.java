78738d63b9d3583766bc7cc342896169f203410c
Rick South <rick@natamus.com>
2026-06-18 23:32:37 +0200

    Updated Piglin Names to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/piglinnames/util/Reference.java
+++ Common/src/main/java/com/natamus/piglinnames/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "piglinnames";
 	public static final String NAME = "Piglin Names";
 	public static final String VERSION = "1.4";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
